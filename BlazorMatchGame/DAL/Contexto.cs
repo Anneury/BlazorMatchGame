239ef9f07f2025c9ea5bfc6c7520a5e6ed971c42
Franlly Mendoza Fana <franlly_mendoza@ucne.edu.do>
2021-07-21 17:07:51 -0400

    agregando clase y BLL de usuarios

--- BlazorMatchGame/DAL/Contexto.cs
+++ BlazorMatchGame/DAL/Contexto.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using BlazorMatchGame.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorMatchGame.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<Usuarios> usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\DBMatchGame.db");
+        }
     }
 }
